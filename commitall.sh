08ef227bf1d9cf84030db4f6e6acd64587268323
Lee <lee.jensen@agilix.com>
2023-01-06 17:15:10 -0700

    enhance(commitall) added simon-login

--- commitall.sh
+++ commitall.sh
@@ -19,8 +19,9 @@ cd ../simon-html && git commit -am "$msg" && git push
 cd ../simon-css && git commit -am "$msg" && git push
 cd ../simon-javascript && git commit -am "$msg" && git push
 cd ../simon-fetch && git commit -am "$msg" && git push
-cd ../simon-db && git commit -am "$msg" && git push
 cd ../simon-service && git commit -am "$msg" && git push
+cd ../simon-db && git commit -am "$msg" && git push
+cd ../simon-login && git commit -am "$msg" && git push
 cd ../simon-react && git commit -am "$msg" && git push
 
 echo cd ../webprogramming260
